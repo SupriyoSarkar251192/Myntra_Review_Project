3c55c5e3868bdfeef32e06564e78c319ba6c097c
Supriyo <supriyosarkar.2884@gmail.com>
2024-09-25 01:27:32 +0530

    New project

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='scrapper',
+    version='0.0.1',
+    author='Supriyo Sarkar',
+    author_email='supriyosarkar.25111992@gmail.com',
+    packages=find_packages(),
+    install_requires = []
+)
